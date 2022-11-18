4257f44c7ea6cea6d95dd5fe00ed6e39d46d49a2
Renan de Oliveira <renan_oliveira93@outlook.com.br>
2022-11-17 21:59:18 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import Header from "./components/header"
 import NotificationButton from "./components/NotificationButton/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
---
+++ frontend/src/components/header/index.tsx
@@ -0,0 +1,17 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+export default function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="logotipo" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://github.com/renan-oliveira93"> Renan de Oliveira</a>
+                </p>
+            </div>
+        </header>
+    )
+}
